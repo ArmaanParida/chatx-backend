9c26ff69444f6f26fb53787a84e6732d25ec31e6
Armaan Parida <162942875+ArmaanParida@users.noreply.github.com>
2026-04-18 10:48:58 +0530

    Refactor websocket implementation in main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import json
+
+app = FastAPI()
+
+connections = []
 
 @app.websocket("/ws")
 async def websocket_endpoint(ws: WebSocket):
@@ -7,8 +12,11 @@ async def websocket_endpoint(ws: WebSocket):
     try:
         while True:
             data = await ws.receive_text()
+
+            # parse incoming JSON
             msg = json.loads(data)
 
+            # broadcast to all except sender
             for conn in connections:
                 if conn != ws:
                     await conn.send_text(json.dumps(msg))
